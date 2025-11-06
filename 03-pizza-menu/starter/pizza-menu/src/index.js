fde95e2eb6abdb5bc0fb4a6faec96ed925dfd19f
Shaheen Shaikh <162513187+FoxNineOne@users.noreply.github.com>
2025-11-06 23:04:29 +0000

    Thursday night push
    completed a few more lessons, tidied up pizza menu folder

---
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { pizzaData } from "./data";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = openHour <= hour && closeHour >= hour;
+  //   if (openHour <= hour && closeHour >= hour) alert("We're currently open!");
+  //   else alert("Sorry, we go sleep sleep");
+
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>
+    //   return React.createElement("footer", null, "We're currently open!");
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas\spinaci.jpg" alt="Pizza Spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>;
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
